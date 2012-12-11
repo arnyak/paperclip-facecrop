2bd560d3e29da699fcf6f0bab62f0cf29e578ff2
Arnauld Nyaku <arnauld.nyaku@c2bsa.com>
2012-12-11 17:01:16 +0100

    Allow paperclip-facecrop play with rails 2 versions

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -1,22 +1,39 @@
 module FaceCrop
-  class Railtie < Rails::Railtie
-    initializer "paperclip-facecrop.extend_has_attachment" do
-      raise "Paperclip needed" unless defined?(Paperclip)
-      ActiveSupport.on_load :active_record do
-           
-        class ActiveRecord::Base
-          
-          class << self
-            def has_attached_file_with_face_crop_cache(name, args)
-              has_attached_file_without_face_crop_cache(name, args)
-              send("after_#{name}_post_process", lambda { FaceCrop::Detector::Cache.clear })
+  if defined? Rails::Railtie
+    class Railtie < Rails::Railtie
+      initializer "paperclip-facecrop.extend_has_attachment" do
+        raise "Paperclip needed" unless defined?(Paperclip)
+        ActiveSupport.on_load :active_record do
+             
+          class ActiveRecord::Base          
+            class << self
+              def has_attached_file_with_face_crop_cache(name, args)
+                has_attached_file_without_face_crop_cache(name, args)
+                send("after_#{name}_post_process", lambda { FaceCrop::Detector::Cache.clear })
+              end
+              
+              alias_method_chain :has_attached_file, :face_crop_cache
             end
-            
-            alias_method_chain :has_attached_file, :face_crop_cache
-          end
-        end        
-      end
+          end        
+        end
       
+      end
+    end
+  end 
+  if defined? Rails && Rails.version =~ /^2\./
+    class Railtie   
+      Rails.configuration.after_initialize do
+        raise "Paperclip needed" unless defined?(Paperclip)
+        class ActiveRecord::Base
+            class << self
+              def has_attached_file_with_face_crop_cache(name, args)
+                has_attached_file_without_face_crop_cache(name, args)
+              end
+
+              alias_method_chain :has_attached_file, :face_crop_cache
+            end
+         end        
+       end    
     end
   end
 end
